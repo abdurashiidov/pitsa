845042120e430034eb99d1a3bb0123035ee5ce4c
abdurashiidov <abdurashiidov18@gmail.com>
2022-10-15 19:53:59 +0300

    add

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ elSelectQan.addEventListener("change", ()=>{
 })
 
 //! size function
-for (let i = 0; i < array.length; i++) {
+for (let i = 0; i < array.length; i++) { 
     if (i == [0]) {
       
     }
